4769ea992d831f43bb22f8977df74c0e40121cc7
zakaria-root <elmourtazakezia@gmail.com>
2022-04-17 18:20:59 +0000

    feat add the implementation of  the remove root implementation  the get smaleest node

--- BSTImplemetation.h
+++ BSTImplemetation.h
@@ -84,3 +84,87 @@ Node_t *BST::getNode(double value)
 {
     return getNodeHelper(root, value);
 }
+double BST::getSmallestNodeHelper(Node_t *temp)
+{
+    if (temp != nullptr)
+    {
+        if (temp->left != nullptr)
+        {
+            getSmallestNodeHelper(temp->left);
+        }
+        else
+        {
+            return temp->value;
+        }
+    }
+    else
+    {
+        std::cout << "l'arbre est vide ..." << std::endl;
+    }
+}
+
+double BST::getSmallestNode()
+{
+    return getSmallestNodeHelper(root);
+}
+
+void BST::removeRootMatche()
+{
+    if (root != nullptr)
+    {
+        double rootValue = root->value;
+        double smallestValue;
+        Node *delptr = root;
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            root = nullptr;
+            delete delptr;
+        }
+        if (root->left == nullptr && root->right != nullptr)
+        {
+            root = root->right;
+            root->right = nullptr;
+            delete delptr;
+            std::cout << "le noeud " << rootValue << "est supprimer et remplacer par le de la valeur " << root->value << std::endl;
+        }
+
+        if (root->left != nullptr && root->right == nullptr)
+        {
+            root = root->left;
+            root->left = nullptr;
+            delete delptr;
+            std::cout << "le noeud " << rootValue << "est supprimer et remplacer par le de la valeur " << root->value << std::endl;
+        }
+
+        if (root->left != nullptr && root->right != nullptr)
+        {
+            smallestValue = getSmallestNodeHelper(root);
+            removeNodeHelper(root, smallestValue);
+            root->value = smallestValue;
+            std::cout << "le noeud " << rootValue << "est supprimer et remplacer par le de la valeur " << root->value << std::endl;
+        }
+    }
+}
+void BST::removeNodeHelper(Node_t *temp, double value)
+{
+    if (temp != nullptr)
+    {
+        if (temp->value == value)
+        {
+            removeRootMatche();
+        }
+        if (value <= temp->value && temp->left != nullptr)
+        {
+            temp->left->value == value ? removeMatche(temp, temp->left, true) : removeNodeHandler(temp->left, value);
+        }
+        else if (value > temp->value && temp->right != nullptr)
+        {
+            temp->right->value == value ? removeMatche(temp, temp->right, true) : removeNodeHandler(temp->right, value);
+        }
+        std::cout << "le node est introuvable" << std::endl;
+    }
+    else
+    {
+        std::cout << "l'arbre est vide ..." << std::endl;
+    }
+}
